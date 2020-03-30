6b50be1229cb2a7410f3df85f8f0d40155489894
Kalfaa <theo92320@hotmail.fr>
2020-03-30 16:23:30 +0200

    Angular project

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {   "transform": {     "^.+\.(t|j)s$": "ts-jest"   },
+    "moduleFileExtensions": [     "js",     "json",     "ts"   ],
+    "rootDir": "./test",
+    "testRegex": "test.ts$",
+    "testEnvironment": "node" };
